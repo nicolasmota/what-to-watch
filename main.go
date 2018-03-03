ca3530046c448a0e9aba213afd36ead4ddd6b65f
nickmota <nicolas_mota@me.com>
2018-03-03 14:17:42 -0300

    Improve results payload and add http tests

--- main.go
+++ main.go
@@ -7,19 +7,30 @@ import (
 	"os"
 )
 
+// Results represents the results returned inside from ResponseJSON
+type Results struct {
+	VoteCount        int     `json:"vote_count"`
+	ID               int     `json:"id"`
+	Video            bool    `json:"video"`
+	VoteAverage      float64 `json:"vote_average"`
+	Title            string  `json:"title"`
+	Popularity       float64 `json:"popularity"`
+	PosterPath       string  `json:"poster_path"`
+	OriginalLanguage string  `json:"original_language"`
+	OriginalTitle    string  `json:"original_title"`
+	GenreIds         []int   `json:"genre_ids"`
+	BackdropPath     string  `json:"backdrop_path"`
+	Adult            bool    `json:"adult"`
+	Overview         string  `json:"overview"`
+	ReleaseDate      string  `json:"release_date"`
+}
+
 // ResponseJSON represents a response returned from themoviedb
 type ResponseJSON struct {
 	Page         int       `json:"page"`
 	TotalResults int       `json:"total_results"`
 	TotalPages   int       `json:"total_pages"`
 	Results      []Results `json:"results"`
-}
-
-// Results represents the results returned inside from ResponseJSON
-type Results struct {
-	Title      string `json:"title"`
-	PosterPath string `json:"poster_path"`
-	Overview   string `json:"overview"`
 }
 
 func main() {
@@ -33,7 +44,7 @@ func main() {
 
 	x := utils.RandomString(1)
 
-	movieDbURL := "https://api.themoviedb.org/3/search/movie/?api_key=" + *apiKey + "&language=pt-BR&query=" + x
+	movieDbURL := fmt.Sprintf("https://api.themoviedb.org/3/search/movie/?api_key=%s&language=pt-BR&query=%s", *apiKey, x)
 
 	resp := new(ResponseJSON)
 	utils.GetJSON(movieDbURL, resp)
